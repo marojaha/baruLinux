0d37c6d93b67c71a85e24d61b46449782a99bd33
marojaha <maroherison@gmail.com>
2023-04-25 19:05:25 +0700

    buat folder

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Selamat Datang di Linux</h1>
+    <div class="container">
+      <div class="container-menu">
+      <button class="menu">Pengumpulan</button>
+      <button class="menu">Pembakaran</button>
+      <button class="menu">Pemilahan</button>
+      <button class="menu">Meteran</button>
+      <button class="menu">Pengangkutan</button>
+      <button class="menu">Ph Air</button>
+      </div>
+      <div class="container-kumpul">
+        <button class="m-kumpul">Sampah</button>
+        <button class="m-kumpul">Limbah Cair</button>
+        <button class="m-kumpul">Limbah B3</button>
+        <button>Limbah B3</button>
+      </div>  
+      <div class="container-angkut">
+        <button class="m-angkut">Sampah</button>
+        <button class="m-angkut">Limbah B3</button>
+
+      </div> 
+    </div>
+    <script src="script.js" ></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('.container');
+const containerMenu = document.querySelector('.container-menu');
+const containerAngkut = document.querySelector('.container-angkut');
+const containerKumpul = document.querySelector('.container-kumpul');
+
+const menus = document.querySelectorAll('.menu');
+const mKumpuls = document.querySelectorAll('.m-kumpul');
+const mAngkut = document.querySelector('.m-angkut');
+
+menus.forEach(function(tombol){
+    tombol.addEventListener('click', function(e){
+        if (e.target.innerText == 'Pengumpulan'){
+        container.style.display = 'none';
+        containerKumpul.style.display='block';
+        }
+    })
+})
