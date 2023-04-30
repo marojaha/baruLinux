d7f7668916a963362e1509d59a73da34bafc84c5
marojaha <maroherison@gmail.com>
2023-04-30 20:46:46 +0700

    membuat scrip ajax

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-const container = document.querySelector('.container');
-const containerMenu = document.querySelector('.container-menu');
-const containerAngkut = document.querySelector('.container-angkut');
-const containerKumpul = document.querySelector('.container-kumpul');
-
-const menus = document.querySelectorAll('.menu');
-const mKumpuls = document.querySelectorAll('.m-kumpul');
-const mAngkut = document.querySelector('.m-angkut');
-
-menus.forEach(function(tombol){
-    tombol.addEventListener('click', function(e){
-        if (e.target.innerText == 'Pengumpulan'){
-        containerMenu.style.display = 'none';
-        containerKumpul.style.display = 'block';
-        }
+$(document).ready(function() {
+    $('#login').click(function() {
+        let userName = $('#userName').val()
+        let password = $('#password').val()
+        $ajax({
+            url: "login.php",
+            type: "POST",
+            data: {
+                "userName": userName,
+                "password": password
+            }
+        })
     })
 })
