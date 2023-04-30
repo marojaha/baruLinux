6b26def6e40cdaf25a63dc7b50f80e5b13793ab3
marojaha <maroherison@gmail.com>
2023-04-30 20:47:29 +0700

    meanmbahkan link

--- index.php
+++ index.php
@@ -12,17 +12,19 @@
     <div class="container">
       <p id="judulLogin">Login</p>
       <form action="">
-        <div class="form-container">
-          <input type="text" id="userName" placeholder="User Name">
-          <input type="password" id="password" placeholder="Password">
-        </div>
-        <div class="controller">
-          <button id="cancel">Cancel</button>
-          <button id="login">Login</button>
-        </div>
+        
       </form>
-    </div>
+      <div class="form-container">
+        <input type="text" id="userName" placeholder="User Name">
+        <input type="password" id="password" placeholder="Password">
+      </div>
+      <div class="controller">
+        <button id="cancel">Cancel</button>
+        <button id="login">Login</button>
+      </div>
 
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <script src="script.js" ></script>
 </body>
 </html>
